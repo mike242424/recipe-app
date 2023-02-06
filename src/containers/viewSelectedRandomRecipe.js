39361203048d3d30904d162c88445e174afe205e
Michael Byers <michaelbyers@Michaels-MacBook-Pro.local>
2023-02-06 18:15:45 -0500

    created render instructions function

--- src/containers/viewSelectedRandomRecipe.js
+++ src/containers/viewSelectedRandomRecipe.js
@@ -1,17 +1,27 @@
-import { useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import _ from 'lodash';
 
 
 const ViewSelectedRandomRecipe = () => {
   const randomRecipeData = useSelector(state => state.randomRecipeData);
-  console.log(randomRecipeData);
+  // console.log(randomRecipeData);
   const { id } = useParams();
   const isRecipe = r => r.id === Number(id);
   const recipe = randomRecipeData.recipes.find(isRecipe);
   console.log(recipe);
 
+  const renderSteps = () => {
+    if (!_.isEmpty(recipe)) {
+      return recipe.analyzedInstructions[0].steps.map((item, i) =>
+      <p key={recipe.id}>{`${i + 1}. ${item.step}`}</p>
+      )
+    }  
+  }
+
   return (
-    <div className="recipe-info text-center">
+    <div className="recipe-info text-center" key={recipe.id}>
       <div className="row">
         <div className="col-8 offset-2">
           <br />
@@ -24,6 +34,7 @@ const ViewSelectedRandomRecipe = () => {
           <h1>{recipe.title}</h1>
           <p>Servings: {recipe.servings}</p>
           <p>Cooking Time: {recipe.readyInMinutes} minutes</p>
+          {renderSteps()}
         </div>
       </div>
     </div>
